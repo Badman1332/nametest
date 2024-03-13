1ed2ca3bb16f37a40d85a602855a2745df7e9442
Badman1332 <mik.davidsson@gmail.com>
2024-03-13 10:28:09 +0100

    Added define method

--- src/main/java/org/company/Namerepository.java
+++ src/main/java/org/company/Namerepository.java
@@ -1,22 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class NameRepository {
 
     private static String[] names = {};
 
-    public static int getSize() {
-        int length = names.length;
-        return length;
+       public static String[] findByFirstName(final String firstName) {
+        List<String> matches = new ArrayList<>();
+        for (String name : names) {
+            if (name.startsWith(firstName + " ")) {
+                matches.add(name);
+            }
+        }
+        return matches.toArray(new String[0]);
     }
 
-    public void setNames(String[] names) {
-        NameRepository.names = names.clone();
+    public static String[] findByLastName(final String lastName) {
+        List<String> matches = new ArrayList<>();
+        for (String name : names) {
+            int indexOfSpace = name.lastIndexOf(" ");
+            if (indexOfSpace != -1 && name.substring(indexOfSpace + 1).equals(lastName)) {
+                matches.add(name);
+            }
+        }
+        return matches.toArray(new String[0]);
     }
 
-    public static void clear() {
-        names = new String[0];
+    public static boolean update(final String original, final String updatedName) {
+        int index = indexOf(original);
+        if (index == -1 || indexOf(updatedName) != -1) {
+            return false;
+        }
+        String[] newNames = Arrays.copyOf(names, names.length);
+        newNames[index] = updatedName;
+        names = newNames;
+        return true;
     }
 
-    public static String[] findAll() {
-        return names.clone();
+    private static int indexOf(String name) {
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals(name)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
+
