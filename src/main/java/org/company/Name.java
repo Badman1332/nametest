7e9fae1c1a521e1cb62821efafc646955c457869
Badman1332 <mik.davidsson@gmail.com>
2024-03-13 10:02:45 +0100

    newrep

--- src/main/java/org/company/Name.java
+++ src/main/java/org/company/Name.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Name {
 
     public static void main(String[] args) {
-        String[] names = {"erik svensson", "anna maria ", "david davidsson"};
+        String[] names = new java.lang.String[]{"erik svensson", "anna maria ", "david davidsson"};
         String[] formattedNames = new String[names.length];
         for (int i = 0; i < names.length; i++) {
             String[] nameParts = names[i].split(" ");
