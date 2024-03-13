af045f7ac94e5a13b1a35364e792128a70790835
Badman1332 <mik.davidsson@gmail.com>
2024-03-13 13:43:37 +0100

    Added define method

--- src/main/java/org/company/NameMethod.java
+++ src/main/java/org/company/NameMethod.java
@@ -6,9 +6,7 @@ class Namerepository {
 
     private static String[] names = {};
 
-    // ... existing methods ...
-
-    public static String find(final String fullName) {
+     public static String find(final String fullName) {
         for (String name : names) {
             if (name.equals(fullName)) {
                 return name;
